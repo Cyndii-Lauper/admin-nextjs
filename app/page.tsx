c3f33fd0cf8055ac8101deea6ee28338ff226f20
Minh <elevenine00@gmail.com>
2024-05-28 17:02:35 +0700

    CRUD Invoices

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,6 @@ export default function Page() {
   return (
     // There are tailwind classes
     <main className="flex min-h-screen flex-col p-6">
-      {/* <div className={styles.shape} /> */}
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         <AcmeLogo />
       </div>
@@ -34,7 +33,6 @@ export default function Page() {
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
           <Image
             src="/hero-desktop.png"
             width={1000}
